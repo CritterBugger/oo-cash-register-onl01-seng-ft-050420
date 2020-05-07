38db2ba954738c22a0b491b2d6c5b9ed18ef061d
Luke Chen <luke.chen.2010@gmail.com>
2020-05-07 19:37:25 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,13 +1,11 @@
 class CashRegister
   
   attr_reader :discount
+  attr_accessor :total
   
   def initialize(discount = 0)
     @total = 0
     @discount = discount
   end
   
-  def total
-    return @total
-  end
 end
